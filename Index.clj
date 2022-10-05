d88d798f8dbc60def1d36d6a03a35fa1ce92c9d8
Chris damiano Kafwafwa <105072048+CHRISDAKAFWA1@users.noreply.github.com>
2022-10-05 12:17:26 +0200

    Add files via upload

---
+++ Index.clj
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"> 
+    <link rel="stylesheet" href="style.css"/>
+    <title>DAKAFWA SITE</title>
+</head>
+<body>
+<!--navbar-->
+
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+  <div class="container">
+    <a href="#" class="navbar-brand">MY SITE</a>
+
+    <button
+       class="navbar-toggler"
+       type="button"
+       data-bs-toggler="collapse"
+       data-bs-target="#navmenu">
+
+       <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+
+    <div class="collapse navbar-collapse" id="#navmenu">
+        <ul class="#navbar-nav ms-auto">
+
+          <li class="nav-items">
+            <a href="#About" class="nav-links">About COMPLICATED LIFESTYLE</a>
+          </li>
+          <li class="nav-items">
+             <a href="#learn" class="nav-links">What to LEARN</a>
+           </li>
+            <li class="nav-items">
+             <a href="#question" class="nav-links">Ask any related QUESTION</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+  </nav>
+<!-- showcase -->
+
+<section class="bg-dark text-center p-5 p-sm-0 pt-5 text-light text-sm-start">
+  <div class="container">
+    <div class="d-sm-flex align-items-center justify-content-between">
+      <div>
+       <h1>COMPLICATED <span class="text-warning"> LIFESTYLE</span></h1>
+      </div>
+
+      <p class="lead my-4">
+
+        We prefer on making this world
+         a better place to live,
+        and teache people how to make this life
+         a hunky dory for everyone,
+        as you know we are in
+         a complicated lifestyle.
+      </p>
+
+
+      <button class="btn btn-primary btn-lg">Join Us</button>
+
+
+     <img class="img-fluid w-50 d-none d-sm-block" src="https://photos.google.com/photo/AF1QipM-Y6Z14nb9MJEDjzE2v5JVqGm79bqi38ksr02j" alt="My photo">
+     
+     
+     
+
+    </div>
+  </div>
+</section>
+
+<!-- Newsletter -->
+<section class="bg-primary text-light p-5">
+  <div class="container">
+    <div class="d-md-flex justify-content-between align-items-center">
+      <h3 class="mb-3 mb-md-0">Sign up for Newsletter</h3>
+
+      <div class="input-group news-input">
+        <input type="text"
+         class="form-control"
+          placeholder="Enter email"
+        />
+
+        <button class="btn btn-dark btn-lg"
+         type="button">Button
+        </button>
+      </div>
+
+
+
+    </div>
+
+  </div>
+</section>
+<!-- boxes -->
+<section class="p-5">
+  <div class="container-fixed">
+    <div class="row text-center">
+      <div class="col-md">
+        <div class="card bg-dark text-light">
+          <div class="h1 mb-3">
+            <i class="bi.bi-laptop"></i>
+          </div>
+          <h3 class="card-title mb-3">virtual</h3>
+             <p class="card-text text-center word-break">
+               Lorem ipsum dolor sit amet,
+               consectetur adipisicing
+               elit. Soluta, ab cupiditate
+               excepturi doloremque
+               facere quis.
+             </p>
+             <a href="#" class="btn btn-primary">Read More</a>
+        </div>
+      </div>
+
+
+      <div class="col-md p-3">
+       <div class="card bg-secondary text-light">
+        <div class="h1 mb-3">
+          <i class="bi.bi-person-square"></i>
+        </div>
+         <h3 class="card-title mb-3">Hybride</h3>
+           <p class="card-text text-center">
+             It is oky to cry when you feel not oky
+             and to hate your own life is oky too
+           </p>
+           <a href="#" class="btn btn-dark">Read More</a>
+       </div>
+       </div>
+
+      </div>
+
+
+      <div class="col-md mt-5">
+        <div class="card bg-dark text-light">
+          <div class="h1 mb-3">
+            <i class="bi.bi-people"></i>
+          </div>
+            <h3 class="card-title mb-3 text-center">in person</h3>
+           <p class="card-text text-center">
+             Kafwafwa ipsum dolor sit amet,
+             consectetur adipisicing
+             elit, Chris-Dakafwa enika voskatan akol. Soluta, ab cupiditate
+             excepturi doloremque
+             facere quis.
+           </p>
+           <a href="#" class="btn btn-danger">Read More</a>
+       </div>
+   
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+</section>
+
+<!--Learn Section-->
+<section id="Learn" class="p-5">
+  <div class="container">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-md">
+        <img src="https://photos.google.com/photo/AF1QipMifEesXN67p9g4c4jZSpjr8UDQGiOdFU5ut2bP" class="img-fluid">
+      </div>
+      <div class="col-md p-5">
+        <h1>Learn the fundamentals</h1>
+        <p class="lead">Lorem ipsum dolor
+           sit amet consectetur adipisicing elit.
+            Optio dolores nostrum libero deleniti
+             alias. Officia?
+        </p>
+        <p>
+          Lorem ipsum dolor,
+           sit amet consectetur adipisicing elit.
+            Impedit laboriosam enim perspiciatis
+             doloremque reiciendis architecto inventore, 
+             nihil deleniti sapiente hic. Nam, facilis aliquam.
+              Harum iure, doloribus provident in necessitatibus earum!
+        </p>
+        <a href="" class="btn btn-light mt-3 bg-secondary">
+          <i class="bi bi-chevron-right">Read more</i>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="Learn" class="p-5 bg-dark text-light">
+  <div class="container">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-md p-5">
+        <h1>Learn the fundamentals</h1>
+        <p class="lead">Lorem ipsum dolor
+           sit amet consectetur adipisicing elit.
+            Optio dolores nostrum libero deleniti
+             alias. Officia?
+        </p>
+        <p>
+          Lorem ipsum dolor,
+           sit amet consectetur adipisicing elit.
+            Impedit laboriosam enim perspiciatis
+             doloremque reiciendis architecto inventore, 
+             nihil deleniti sapiente hic. Nam, facilis aliquam.
+              Harum iure, doloribus provident in necessitatibus earum!
+        </p>
+        <a href="" class="btn btn-light mt-3 bg-primary">
+          <i class="bi bi-chevron-right">Read more</i>
+        </a>
+      </div><div class="col-md">
+        <img src="https://photos.google.com/photo/AF1QipPYng8VEGetufTYPGheTvN6zzjWTRBykuysovn7" class="img-fluid">
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!-- Question -->
+<section id="Question" class="p-5">
+  <div class="container">
+    <h2 class="text-center mb-4">Frequent Asked Question</h2>
+        <!--item 1-->
+          <button class="accordion-button collapsed" type="button"
+           data-bs-toggle="collapse" data-bs-target="#Question">
+            Do you know Chris-Dakafwa?
+          </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse"
+         data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">Placeholder content for this accordion,
+             which is intended to demonstrate the <code>.accordion-flush</code> 
+             class. This is the first item's accordion body.</div>
+        </div>
+      </div>
+        <!--item 2-->
+          <button class="accordion-button collapsed" type="button"
+           data-bs-toggle="collapse" data-bs-target="#question">
+            Do you really once fuck with him?
+          </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse"
+         data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">Yeah! I did once fucked with him and not
+             only once but for so many times</div>
+        </div>
+      </div>
+        <!--item 3-->
+          <button class="accordion-button collapsed" type="button"
+           data-bs-toggle="collapse" data-bs-target="#question">
+             How was the service! Did you like it?
+          </button>
+        </h2>
+        <div id="#Q" class="accordion-collapse collapse"
+          data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">It is the memont I will never forget in my life,
+             he fucked me like gell[prison]. 
+             I know it will take time to forget how he did the job</div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+
+</section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>   
+</body>
+    
+</html> 
